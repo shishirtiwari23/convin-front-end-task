03eb9148f6c762c7848aff525933e6ae50a23125
shishirtiwari23 <shishir456tiwari@gmail.com>
2023-01-06 19:56:07 +0530

    inital commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,10 @@ import { useSelector, useDispatch } from "react-redux";
 import { baseUserURL, userActions } from "./store";
 
 const App = () => {
+  const [isPageLoading, setIsPageLoading] = useState(true);
   const [currentUser, setCurrentUser] = useState(null);
   const [currentUserId, setCurrentUserId] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
   const users = useSelector((state) => state?.userSlice?.users);
   const dispatch = useDispatch();
 
@@ -14,8 +16,10 @@ const App = () => {
     async function getUser() {
       if (currentUserId) {
         try {
+          setIsLoading(true);
           const res = await axios.get(`${baseUserURL}/users/${currentUserId}`);
           setCurrentUser(res?.data?.data);
+          setIsLoading(false);
         } catch (err) {
           console.log(err);
         }
@@ -25,32 +29,90 @@ const App = () => {
     }
     getUser();
   }, [currentUserId]);
+
   useEffect(() => {
-    console.log(currentUser);
+    console.log(isLoading);
   });
+
   useEffect(() => {
     async function getAllUsers() {
       try {
         const res = await axios.get(`${baseUserURL}/users`);
         dispatch(userActions.updateUsers(res?.data?.data));
+        setIsPageLoading(false);
       } catch (err) {
         console.log(err);
       }
     }
     getAllUsers();
   }, []);
+
+  if (isPageLoading)
+    return (
+      <div className="loadingContainer">
+        <svg viewBox="25 25 50 50">
+          <circle r="20" cy="50" cx="50"></circle>
+        </svg>
+      </div>
+    );
+
   return (
     <div className="App">
-      <div className="userInfo card">
-        <h1>Current User Id {currentUserId}</h1>
+      <div className={currentUser ? "userInfo card" : "noUser card"}>
+        {currentUser ? (
+          <>
+            {isLoading ? (
+              <div className="cardLoadingContainer">
+                <svg viewBox="25 25 50 50">
+                  <circle r="20" cy="50" cx="50"></circle>
+                </svg>
+              </div>
+            ) : (
+              <>
+                <div className="left">
+                  <h3 className="name">
+                    {currentUser?.first_name} {currentUser?.last_name}
+                  </h3>
+                  <h4 className="id">Id: {currentUser?.id}</h4>
+                  <h4 className="email">Email: {currentUser?.email}</h4>
+                </div>
+                <div className="right">
+                  <img
+                    className="image"
+                    src={currentUser?.avatar}
+                    alt={currentUser?.first_name}
+                  />
+                </div>
+              </>
+            )}
+          </>
+        ) : (
+          <>
+            <h2>Select a user</h2>
+            <h4>Click on one of the button below</h4>
+          </>
+        )}
       </div>
-      <div>
+      <div className="buttons">
         {users.map((user, index) => (
-          <button onClick={() => setCurrentUserId(user.id)} key={index}>
-            User {index + 1}
+          <button
+            className={currentUserId === user?.id ? "active" : ""}
+            onClick={() => setCurrentUserId(user.id)}
+            key={index}
+          >
+            {index + 1}
           </button>
         ))}
       </div>
+      <button
+        className="reset"
+        onClick={() => {
+          setCurrentUserId(null);
+          setCurrentUser(null);
+        }}
+      >
+        Reset
+      </button>
     </div>
   );
 };
